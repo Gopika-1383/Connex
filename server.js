d1b2d324a0dfef6b65eb7569dec18c4c0cbdc082
Gopika <122785613+Gopika-1383@users.noreply.github.com>
2023-04-16 10:55:47 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,47 @@
+let socket = new WebSocket("wss://javascript.info/article/websocket/demo/hello");
+
+socket.onopen = function(e) {
+    alert("[open] Connection established");
+    alert("Sending to server");
+    socket.send("My name is John");
+  };
+  
+  socket.onmessage = function(event) {
+    alert(`[message] Data received from server: ${event.data}`);
+  };
+  
+  socket.onclose = function(event) {
+    if (event.wasClean) {
+      alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      alert('[close] Connection died');
+    }
+  };
+  
+  socket.onerror = function(error) {
+    alert(`[error]`);
+  };
+  
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const port = 6969;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    })
+  })
+})
+
+server.listen(port, function() {
+  console.log(`Server is listening on ${port}!`)
+})
